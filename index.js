d55089fae747e85bd2b19099e185d5bc2d4c8174
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-11-08 18:53:21 -0800

    start to implement createArticle

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var FireBlog = module.exports = function FireBlog() {
+function FireBlog(firebaseRef, options) {
+  this._firebase = firebaseRef;
 
+  this._articleRef = this._firebase.child('blog-articles');
+  this._commentRef = this._firebase.child('blog-comment');
+
+  this._options = options || {};
+
+  this._articleRef = this._firebase.child('article-metadata');
+}
+
+FireBlog.prototype.createArticle = function(articleTitle, callback) {
+
+  var newArticleRef = this._articleRef.push();
+
+  var newArticle = {
+    id: newArticleRef.key(),
+    name: articleTitle
+  };
+
+  newArticleRef.set(newArticle, function(error) {
+    if (error) {
+      callback(error);
+      return;
+    }
+    callback(null, newArticleRef.key());
+  });
 };
+
+
+module.exports = FireBlog;
